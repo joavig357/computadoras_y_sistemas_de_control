5ca4ba93b558e3ff8119d4d631c75a914c98679a
joavig357 <102058456+joavig357@users.noreply.github.com>
2024-05-09 11:16:20 -0300

    Update init.c

--- actividades/actividad_2/libs/init.c
+++ actividades/actividad_2/libs/init.c
@@ -1,12 +1,9 @@
 #include "init.h"
-#include "hardware/pwm.h"
 
 // Variable para guardar el valor de temperatura
 float temperatura = 0.0;
 // Variable para guardar el valor del setpoint
 float setpoint = 0.0;
-// Creo un repeating timer
-struct repeating_timer timer;
 
 /*
  * @brief Inicializacion de perifericos
@@ -14,10 +11,12 @@ struct repeating_timer timer;
  void init(void) {
   // Inicializacion de UART
   stdio_init_all();
+  // Creo un repeating timer
+  struct repeating_timer timer;
   // Creo un callback para la interrupcion del timer
   add_repeating_timer_ms(ADC_DELAY_MS, muestreo_periodico, NULL, &timer);
   // Configuro el I2C0 a 100 KHz de clock
-  i2c_init(i2c0, SCL_FREQ);
+  i2c_init(i2c0, 100 * 1000);
   // Elijo GPIO4 como linea de SDA
   gpio_set_function(SDA_GPIO, GPIO_FUNC_I2C);
   // Elijo GPIO5 como linea de SCL
@@ -44,6 +43,7 @@ struct repeating_timer timer;
   // - 100 Hz de frecuencia
   // - Ancho de pulso inicial del 0%
 
+  
 }
 
 /*
@@ -54,41 +54,38 @@ bool muestreo_periodico(struct repeating_timer *t) {
   // Canal elegido, no se pierde el valor aunque sea local (static)
   static uint8_t selected_channel = 0;
   // Constante de proporcionalidad de NTC
-  const float beta = 3950.0;
+  const uint16_t beta = 3950;
+  // Actualizar el canal seleccionado
+  selected_channel = selected_channel ^ 1;
   // Cambio de canal 0 a 1 o viceversa
-  selected_channel ^= 1;
   adc_select_input(selected_channel);
   // Lectura analogica (variable adc_value)
   uint16_t adc_value = adc_read();
   // Verifico que canal lei
-  if(selected_channel == 1) {
-    // Canal 1: termistor. Calcular valor de temperatura (variable temperatura)
+  if(selected_channel == 0) {
+    // Canal 0: termistor. Calcular valor de temperatura (variable temperatura)
     temperatura = 1 / (log(1 / (4095. / adc_value - 1)) / beta + 1.0 / 298.15) - 273.15;
   }
   else {
-    // Canal 0: potenciometro. Calcular el valor de temperatura equivalente (4095 = 35 grados)
-    
-  }
-
-  // Verifico que se hayan leido ambos canales antes de activar el control
-  if(setpoint != 0.0 && temperatura != 0.0) {
+    // Canal 1: potenciometro. Calcular el valor de temperatura equivalente (4095 = 35 grados)
+    setpoint = adc_value*35/4095 
     // Calcular error (temperatura - setpoint)
     float error = temperatura - setpoint;
     // Llamar al control proporcional
-    int16_t control_p = saturador(control_proporcional(error), PWM_WRAP);
+    float control_p = control_proporcional(error);
     // Verificar el signo del control
-    if((int8_t)temperatura < (int8_t)setpoint) {
-      // Si es negativo: temperatura < setpoint -> caliento
+    if(control_p < 0) {
+      // Si es negativo: temperatura > setpoint -> enfrio
       // Apagar un canal de PWM
-
-    }
-    else if((int8_t)temperatura > (int8_t)setpoint) {
-      // Si es positivo: temperatura < setpoint -> enfrio
-      // Apagar un canal de PWM
-
+      
+      // Encender el otro
+      
     }
     else {
-      // Apagar canales de PWM
+      // Si es positivo: temperatura < setpoint -> caliento
+      // Apagar un canal de PWM
+
+      // Encender el otro
 
     }
   }
